14b7274c66bd8217672d1d25016f265eef6114db
Moritz Gruber <moritzgruber@yahoo.de>
2016-04-24 20:01:03 +0200

    android is working too, i needed to add a whitlist plugin, its just for testing, laiter do the forwarding explicit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#adding dependencies
+sudo npm install
+sudo bower install --allow-root
+#installing plugins
+sudo cordova plugin add cordova-plugin-whitelist
+#adding platfroms
+sudo ionic platform add ios android
