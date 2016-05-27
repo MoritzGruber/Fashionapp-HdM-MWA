28aa5a4507761e8b68be72ba7c0373ccd1d37987
MoritzGruber <moritzgruber@yahoo.de>
2016-05-27 22:16:23 +0200

    added cordova hockeyapp plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,37 +25,41 @@ angular.module('starter.controllers', [])
       }
     });
   })
-.controller('PhotoCtrl', function($scope, $base64, socket, Camera, storage, $localStorage, $state, voteservice) {
-    console.log(storage.getNumber());
-      if (storage.getNumber().length <3 || storage.getNumber().length >10 || storage.getNumber() == "Unknown" ) {
-          //app opend the first time ==> go to welcome page
-          $state.go('tab.collectionstart');
-      }
+.controller('PhotoCtrl', function($scope, $base64, socket, Camera, storage, $localStorage, $ionicPlatform, $state, voteservice) {
+  $ionicPlatform.ready(function() {
+    hockeyapp.start(null, null, "92590608ebe64ac682e3af9bb46019cd");
+    hockeyapp.checkForUpdate();
+  });
+  console.log(storage.getNumber());
+  if (storage.getNumber().length <3 || storage.getNumber().length >10 || storage.getNumber() == "Unknown" ) {
+      //app opend the first time ==> go to welcome page
+      $state.go('tab.collectionstart');
+  }
 
-      $scope.getPhoto = function() {
-        //first we define a var to set the settings we use calling the cordova camera,
-        var cameraSettings = {
-          sourceType: 1, //navigator.camera.PictureSourceType.CAMERA,
-          destinationType: 0, //navigator.camera.DestinationType.DATA_URL, // very importend!!! to get base64 and no link NOTE: mybe cause out of memory error after a while
-          quality: 75,
-          targetWidth: 320,
-          targetHeight: 320,
-          saveToPhotoAlbum: true,
-        };
-        //calling our service with asynchronously runs the cordova camera plugin
-       Camera.getPicture(cameraSettings).then(function(imageData) {
-          //adding the phone number and pasing the object to json
-          var votes = [];
-          var image= {"imageData":imageData, "timestamp": Date.parse(Date()), "transmitternumber":storage.getNumber(), "recipients":storage.getFriendswithbenefits(), "votes":votes};
-          //upload the image with our open socket connection
-          socket.emit('new_image',(image));
-          //store localy now
-          storage.addOwnImage(image);
-       }, function(err) {
-         console.log(err);
-        //this function dosnt even get called, have to make a cetch outside before
-      });
-     };
+  $scope.getPhoto = function() {
+    //first we define a var to set the settings we use calling the cordova camera,
+    var cameraSettings = {
+      sourceType: 1, //navigator.camera.PictureSourceType.CAMERA,
+      destinationType: 0, //navigator.camera.DestinationType.DATA_URL, // very importend!!! to get base64 and no link NOTE: mybe cause out of memory error after a while
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: true,
+    };
+    //calling our service with asynchronously runs the cordova camera plugin
+   Camera.getPicture(cameraSettings).then(function(imageData) {
+      //adding the phone number and pasing the object to json
+      var votes = [];
+      var image= {"imageData":imageData, "timestamp": Date.parse(Date()), "transmitternumber":storage.getNumber(), "recipients":storage.getFriendswithbenefits(), "votes":votes};
+      //upload the image with our open socket connection
+      socket.emit('new_image',(image));
+      //store localy now
+      storage.addOwnImage(image);
+   }, function(err) {
+     console.log(err);
+    //this function dosnt even get called, have to make a cetch outside before
+  });
+ };
      if ($localStorage.ownImages == undefined) {
          $localStorage.ownImages = [];
      }
@@ -172,6 +176,7 @@ angular.module('starter.controllers', [])
     $scope.storage = storage;
     $scope.number = $localStorage.ownnumber;
     $scope.sendFeedback = function () {
+      hockeyapp.forceCrash();
       socket.emit('feedback', $scope.feedback);
       console.log($scope.feedback);
       $scope.feedback = "";
