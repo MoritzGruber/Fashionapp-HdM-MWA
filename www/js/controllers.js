f983f27970cc2dfb92653212ff975d703935ee88
MoritzGruber <moritzgruber@yahoo.de>
2016-06-08 16:41:05 +0200

    register push test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,35 @@
 angular.module('starter.controllers', [])
-.controller('StartCtrl', function($scope, $css, storage, $state, socket){
-  //hockeyapp.trackEvent(null, null, "at_tab_start");
-  $scope.storage = storage;
-  $scope.start = function () {
-    if (storage.getNumber().length <3 || storage.getNumber().length >10 || storage.getNumber() == "Unknown" ){
-      alert("Please choose a Nickname between 3 and 10 letters");
-    }else{
-      //socket emit new user
-      socket.emit('new_user',(storage.getNumber()));
-      //some unique sucess could be received here
-      //navigate to normal start screen
-      $state.go('tab.collection');
+  .controller('StartCtrl', function ($scope, $css, storage, $state, socket) {
+    //hockeyapp.trackEvent(null, null, "at_tab_start");
+    $scope.storage = storage;
+    $scope.start = function () {
+      if (storage.getNumber().length < 3 || storage.getNumber().length > 10 || storage.getNumber() == "Unknown") {
+        alert("Please choose a Nickname between 3 and 10 letters");
+      } else {
+        //socket emit new user
+      var push = new Ionic.Push({
+        "onNotification": function (notification) {
+          alert('Receved a Notification!');
+        },
+        "pluginConfig": {
+          "android":{
+            "iconColor": "0000FF"
+          }
+        }
+   	  });
+    var callback = function(pushToken) {
+      console.log('Registered token:', pushToken.token);
+      $scope.token = pushToken.token;
     }
-  };
-})
-  .controller('TabsCtrl', function($scope, $rootScope, $state) {
+    push.register(callback);
+        socket.emit('new_user', (storage.getNumber()), $scope.token);
+        //some unique sucess could be received here
+        //navigate to normal start screen
+        $state.go('tab.collection');
+      }
+    };
+  })
+  .controller('TabsCtrl', function ($scope, $rootScope, $state) {
     //this controller disables the tab navigation bar for certain views/tabs
     $rootScope.$on('$ionicView.beforeEnter', function () {
 
@@ -26,98 +41,124 @@ angular.module('starter.controllers', [])
       }
     });
   })
-.controller('PhotoCtrl', function($scope, $base64, $timeout, socket, Camera, storage, $localStorage, $ionicPlatform, $state, voteservice, communicationservice) {
-  $ionicPlatform.ready(function() {
-    if  (!ionic.Platform.platform() == "macintel"){
-      hockeyapp.start(null, null, "92590608ebe64ac682e3af9bb46019cd");
-    }
-    //hockeyapp.checkForUpdate();
-    //hockeyapp.trackEvent(null, null, "at_tab_collection");
-    if (storage.getNumber().length <3 || storage.getNumber().length >10 || storage.getNumber() == "Unknown" ) {
-      //app opend the first time ==> go to welcome page
-      $state.go('tab.collectionstart');
-    }
+  .controller('PhotoCtrl', function ($scope, $base64, $timeout, socket, Camera, storage, $localStorage, $ionicPlatform, $state, voteservice, communicationservice) {
+    $ionicPlatform.ready(function () {
+      //ionic.io deploy platfrom code ::
+      var deploy = new Ionic.Deploy();
 
-  });
-  console.log(storage.getNumber());
-  //functions
-  //remove an Item
-  $scope.removeItem = function(index){
-    console.log("pls add remove action here");
-    //call this to delete the image:
-    //storage.deleteOwnImage(index);
-    
-    //TODO: Some Animation and Delete Button to show up
-  };
-  //switch to the detail view of the selected image
-  $scope.openDetailImage = function(index){
-    console.log("taped");
-    $state.go('tab.collection-detail', {imageId: index});
-  };
-  $scope.getPhoto = function() {
-    if ($scope.collage == true){
-       $scope.collageorder++; // 1,2,3,4 == number in the collage and define that is acutally part of collage
-       if($scope.collageorder < 4 ){
-         $scope.collageorder = 1;
-       }
-    }else{
-      $scope.collageorder == 0;//  0==no collage
-    }
+      // Update app code with new release from Ionic Deploy
+      $scope.doUpdate = function () {
+        deploy.update().then(function (res) {
+          console.log('Ionic Deploy: Update Success! ', res);
+        }, function (err) {
+          console.log('Ionic Deploy: Update error! ', err);
+        }, function (prog) {
+          console.log('Ionic Deploy: Progress... ', prog);
+        });
+      };
 
-    //first we define a var to set the settings we use calling the cordova camera,
-    var cameraSettings = {
-      sourceType: 1, //navigator.camera.PictureSourceType.CAMERA,
-      destinationType: 0, //navigator.camera.DestinationType.DATA_URL, // very importend!!! to get base64 and no link NOTE: mybe cause out of memory error after a while
-      quality: 100,
-      targetWidth: 640,
-      targetHeight: 1136,
-      saveToPhotoAlbum: true,
+      // Check Ionic Deploy for new code
+      $scope.checkForUpdates = function () {
+        console.log('Ionic Deploy: Checking for updates');
+        deploy.check().then(function (hasUpdate) {
+          console.log('Ionic Deploy: Update available: ' + hasUpdate);
+          $scope.hasUpdate = hasUpdate;
+        }, function (err) {
+          console.error('Ionic Deploy: Unable to check for updates', err);
+        });
+      }
+      //end of ionic.io platofrm deploy code
+
+      if (!ionic.Platform.platform() == "macintel") {
+        hockeyapp.start(null, null, "92590608ebe64ac682e3af9bb46019cd");
+      }
+      //hockeyapp.checkForUpdate();
+      //hockeyapp.trackEvent(null, null, "at_tab_collection");
+      if (storage.getNumber().length < 3 || storage.getNumber().length > 10 || storage.getNumber() == "Unknown") {
+        //app opend the first time ==> go to welcome page
+        $state.go('tab.collectionstart');
+      }
+
+    });
+    console.log(storage.getNumber());
+    //functions
+    //remove an Item
+    $scope.removeItem = function (index) {
+      console.log("pls add remove action here");
+      //call this to delete the image:
+      //storage.deleteOwnImage(index);
+
+      //TODO: Some Animation and Delete Button to show up
     };
-    //calling our service with asynchronously runs the cordova camera plugin
-   Camera.getPicture(cameraSettings).then(function(imageData) {
-     
-      //adding the phone number and pasing the object to json
-      var votes = [];
-      var image= {"imageData":imageData, "timestamp": Date.parse(Date()), "transmitternumber":storage.getNumber(), "recipients":storage.getFriendswithbenefits(), "votes":votes, "collageorder":$scope.collageorder};
-      //upload the image with our open socket connection
-      socket.emit('new_image',(image));
-      //store localy now
-      storage.addOwnImage(image);
-     $scope.collage = true;
-     setTimeout(function () {
-       console.log("dudu");
-       $scope.collage = false;
-       $scope.$apply();
-     }, 3000);
+    //switch to the detail view of the selected image
+    $scope.openDetailImage = function (index) {
+      console.log("taped");
+      $state.go('tab.collection-detail', { imageId: index });
+    };
+    $scope.getPhoto = function () {
+      if ($scope.collage == true) {
+        $scope.collageorder++; // 1,2,3,4 == number in the collage and define that is acutally part of collage
+        if ($scope.collageorder < 4) {
+          $scope.collageorder = 1;
+        }
+      } else {
+        $scope.collageorder == 0;//  0==no collage
+      }
 
-   }, function(err) {
-     console.log(err);
-    //this function dosnt even get called, have to make a ceetch outside before
-  });
+      //first we define a var to set the settings we use calling the cordova camera,
+      var cameraSettings = {
+        sourceType: 1, //navigator.camera.PictureSourceType.CAMERA,
+        destinationType: 0, //navigator.camera.DestinationType.DATA_URL, // very importend!!! to get base64 and no link NOTE: mybe cause out of memory error after a while
+        quality: 100,
+        targetWidth: 640,
+        targetHeight: 1136,
+        saveToPhotoAlbum: true,
+      };
+      //calling our service with asynchronously runs the cordova camera plugin
+      Camera.getPicture(cameraSettings).then(function (imageData) {
 
- };
-     if ($localStorage.ownImages == undefined) {
-         $localStorage.ownImages = [];
-     }
-     $scope.ownImages = $localStorage.ownImages;
-     //calling the calculate percentage function for each image
-     for (var i = 0; i < $scope.ownImages.length; i++) {
-         $scope.ownImages[i].percantag = voteservice.getPercentage($scope.ownImages[i].votes);
-     }
-     //TODO: replace the image data with an id
-     socket.on('vote_sent_from_server', function (package) {
-         for (var i = 0; i < $localStorage.ownImages.length; i++) {
-             if ($localStorage.ownImages[i].imageData == package.imageData) {
-                 var vote={"name":package.number, "vote":package.rating};
-                 $localStorage.ownImages[i].votes.push(vote);
-                $localStorage.ownImages[i].percantag = voteservice.getPercentage($localStorage.ownImages[i].votes);
-             }
-         }
-       $scope.ownImages = $localStorage.ownImages;
-     });
-     socket.on('updateUserData', function(data){
-       console.log(data);
-     });
+        //adding the phone number and pasing the object to json
+        var votes = [];
+        var image = { "imageData": imageData, "timestamp": Date.parse(Date()), "transmitternumber": storage.getNumber(), "recipients": storage.getFriendswithbenefits(), "votes": votes, "collageorder": $scope.collageorder };
+        //upload the image with our open socket connection
+        socket.emit('new_image', (image));
+        //store localy now
+        storage.addOwnImage(image);
+        $scope.collage = true;
+        setTimeout(function () {
+          console.log("dudu");
+          $scope.collage = false;
+          $scope.$apply();
+        }, 3000);
+
+      }, function (err) {
+        console.log(err);
+        //this function dosnt even get called, have to make a ceetch outside before
+      });
+
+    };
+    if ($localStorage.ownImages == undefined) {
+      $localStorage.ownImages = [];
+    }
+    $scope.ownImages = $localStorage.ownImages;
+    //calling the calculate percentage function for each image
+    for (var i = 0; i < $scope.ownImages.length; i++) {
+      $scope.ownImages[i].percantag = voteservice.getPercentage($scope.ownImages[i].votes);
+    }
+    //TODO: replace the image data with an id
+    socket.on('vote_sent_from_server', function (package) {
+      for (var i = 0; i < $localStorage.ownImages.length; i++) {
+        if ($localStorage.ownImages[i].imageData == package.imageData) {
+          var vote = { "name": package.number, "vote": package.rating };
+          $localStorage.ownImages[i].votes.push(vote);
+          $localStorage.ownImages[i].percantag = voteservice.getPercentage($localStorage.ownImages[i].votes);
+        }
+      }
+      $scope.ownImages = $localStorage.ownImages;
+    });
+    socket.on('updateUserData', function (data) {
+      console.log(data);
+    });
     //  socket.on('vote_sent_from_server', function (package) {
     //      for (var i = 0; i < $localStorage.ownImages.length; i++) {
     //          if ($localStorage.ownImages[i].imageData == package.imageData) {
@@ -137,10 +178,10 @@ angular.module('starter.controllers', [])
     //          }
     //      }
     //  });
-    $scope.doRefresh = function() {
+    $scope.doRefresh = function () {
 
       communicationservice.updateData("collection");
-      $timeout( function() {
+      $timeout(function () {
         //simulate async response
         //Stop the ion-refresher from spinning
         $scope.$broadcast('scroll.refreshComplete');
@@ -148,30 +189,30 @@ angular.module('starter.controllers', [])
       }, 1000);
 
     };
-})
+  })
 
-.controller('CommunityCtrl', function($scope, socket, $ionicPlatform, $timeout, storage, $localStorage, voteservice, communicationservice) {
-    $scope.getTypeOf = function(value) {
+  .controller('CommunityCtrl', function ($scope, socket, $ionicPlatform, $timeout, storage, $localStorage, voteservice, communicationservice) {
+    $scope.getTypeOf = function (value) {
       console.log("called get type");
-        if(value === null) {
-            return "null";
-        }
-        if(Array.isArray(value)) {
-            return "array";
-        }
-        return typeof value; 
+      if (value === null) {
+        return "null";
+      }
+      if (Array.isArray(value)) {
+        return "array";
+      }
+      return typeof value;
     };
     //hockeyapp.trackEvent(null, null, "at_tab_community");
     console.log("platform: " + ionic.Platform.platform());
     console.log(Date.parse(Date()));
     //this function is called when you hit a vote button
-    $scope.vote= function (voting, indexofvotedimage) {
-            voteservice.vote(voting, indexofvotedimage);
+    $scope.vote = function (voting, indexofvotedimage) {
+      voteservice.vote(voting, indexofvotedimage);
     }
-    $scope.doRefresh = function() {
+    $scope.doRefresh = function () {
 
       console.log('Refreshing!');
-      $timeout( function() {
+      $timeout(function () {
         //simulate async response
         //TODO: call refresh function here
         communicationservice.updateData("community");
@@ -182,22 +223,22 @@ angular.module('starter.controllers', [])
 
     };
 
-    $ionicPlatform.ready(function() {
-        storage.clearOldImages();
-        $scope.local = $localStorage.images;
+    $ionicPlatform.ready(function () {
+      storage.clearOldImages();
+      $scope.local = $localStorage.images;
 
-        //on startup load iamges from storage, if there is sth to load
-        //saving reciving images to scope and storage
+      //on startup load iamges from storage, if there is sth to load
+      //saving reciving images to scope and storage
       socket.on('incoming_image', function (image) {
-          //only call this part if the app is not running the background task
-        if  (ionic.Platform.platform() == "macintel"){
+        //only call this part if the app is not running the background task
+        if (ionic.Platform.platform() == "macintel") {
           storage.addImage(image);
-          $scope.local=$localStorage.images;
+          $scope.local = $localStorage.images;
           $scope.$apply();
-        }else {
+        } else {
           if (!(cordova.plugins.backgroundMode.isActive())) {
             storage.addImage(image);
-            $scope.local=$localStorage.images;
+            $scope.local = $localStorage.images;
             $scope.$apply();
             console.log("saved foreground");
             console.log(cordova.plugins.backgroundMode);
@@ -206,51 +247,51 @@ angular.module('starter.controllers', [])
 
       });
       if (ionic.Platform.platform() == "android" || ionic.Platform.platform() == "ios") {
-            // cordova.plugins.backgroundMode.setDefaults({
-            //     title:  'Lapica',
-            //     text:   'Waiting from friends to post new image'
-            // });
-            // Enable background mode only if there is no other backgroundprozess already running
-            if (!cordova.plugins.backgroundMode.isEnabled()){
-                cordova.plugins.backgroundMode.enable();
-            }
+        // cordova.plugins.backgroundMode.setDefaults({
+        //     title:  'Lapica',
+        //     text:   'Waiting from friends to post new image'
+        // });
+        // Enable background mode only if there is no other backgroundprozess already running
+        if (!cordova.plugins.backgroundMode.isEnabled()) {
+          cordova.plugins.backgroundMode.enable();
+        }
 
 
-            // Called when background mode has been activated
-            cordova.plugins.backgroundMode.onactivate = function () {
-                if (cordova.plugins.backgroundMode._isActive) {
-                    socket.on('incoming_image', function (image) {
+        // Called when background mode has been activated
+        cordova.plugins.backgroundMode.onactivate = function () {
+          if (cordova.plugins.backgroundMode._isActive) {
+            socket.on('incoming_image', function (image) {
 
-                      if (cordova.plugins.backgroundMode._isActive) {
-                        //added workaround for buggy background pluign, but after too many times reopening the app will crash anyway
-                          var i = $localStorage.images.length;
-                        containsbool = false;
-                          while (i--) {
-                            if ($localStorage.images[i] === image) {
-                              containsbool =true;
+              if (cordova.plugins.backgroundMode._isActive) {
+                //added workaround for buggy background pluign, but after too many times reopening the app will crash anyway
+                var i = $localStorage.images.length;
+                containsbool = false;
+                while (i--) {
+                  if ($localStorage.images[i] === image) {
+                    containsbool = true;
 
-                            }
-                          }
-                          if (containsbool == false){
-                            storage.addImage(image);
-                            $scope.bgmode = true;
-                            $scope.local=$localStorage.images;
-                            $scope.$apply();
-                          }
-                        containsbool=true;
-                      }
-                    });
+                  }
                 }
-            }
+                if (containsbool == false) {
+                  storage.addImage(image);
+                  $scope.bgmode = true;
+                  $scope.local = $localStorage.images;
+                  $scope.$apply();
+                }
+                containsbool = true;
+              }
+            });
+          }
+        }
       }
     });
-})
+  })
 
-.controller('ProfileCtrl', function($scope, $localStorage, storage, socket, communicationservice) {
+  .controller('ProfileCtrl', function ($scope, $localStorage, storage, socket, communicationservice) {
     //hockeyapp.trackEvent(null, null, "at_tab_profile");
     // $scope.friends = $localStorage.friends;
     $scope.storage = storage;
-    $scope.updateData = function(){
+    $scope.updateData = function () {
       communicationservice.updateData("profile");
     };
     $scope.number = $localStorage.ownnumber;
@@ -258,26 +299,26 @@ angular.module('starter.controllers', [])
       hockeyapp.feedback();
       console.log("feedback called");
     }
-})
+  })
 
-.controller('CollectionCtrl', function(
-){})
+  .controller('CollectionCtrl', function (
+  ) { })
 
-.controller('CollectionDetailCtrl', function($scope, $stateParams, storage) {
-  $scope.image = storage.getOwnImage($stateParams.imageId);
-})
+  .controller('CollectionDetailCtrl', function ($scope, $stateParams, storage) {
+    $scope.image = storage.getOwnImage($stateParams.imageId);
+  })
 
-.controller('FriendSelectCtrl', function ($scope, storage, $ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        $scope.contacts = storage.getContacts();
+  .controller('FriendSelectCtrl', function ($scope, storage, $ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      $scope.contacts = storage.getContacts();
     });
     //  save/delete contact to localStorage
-    $scope.checkFriend = function(index, number, value) {
-        storage.editFriend(index, number, value);
+    $scope.checkFriend = function (index, number, value) {
+      storage.editFriend(index, number, value);
     }
     //fetch all data from the phone again
     $scope.updateContacts = function () {
-        console.log("updated");
-        $scope.contacts = storage.loadContacts();
+      console.log("updated");
+      $scope.contacts = storage.loadContacts();
     }
-});
+  });
