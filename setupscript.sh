6df254e76b04dc5f65bf0e4314f8723cd4b909ea
Moritz Gruber <moritz@Moritzs-MacBook-Air.local>
2016-11-26 20:17:01 +0100

    added docker stuff and changed tests

--- setupscript.sh
+++ setupscript.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 npm install
 bower install
-ionic state reset
-ionic resources
 ionic serve
--- www/test/storageserviceTest.js
+++ www/test/storageserviceTest.js
@@ -1,16 +1,8 @@
-describe('StorageService', function () {
+describe('supportservice', function () {
   beforeEach(module('starter'));
 
     it('says hello world!', function () {
       expect(true).toEqual(true);
     });
-  it("@storage, getNumber()", inject(function (storage) {
-    expect(storage.getNumber()).toEqual('Unknown');
-  }));
-  it("@storageService, initDB()", inject(function (storageService) {
-    storageService.initDB();
-    expect(true).toEqual(true);
-  }));
-
   });
 
