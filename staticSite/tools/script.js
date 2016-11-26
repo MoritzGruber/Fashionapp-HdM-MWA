739e9e3d65480e2f29c4f7f039be03890b361606
Christoph <chrisi.muth@gmx.de>
2016-11-26 21:53:29 +0100

    fixed api key

--- staticSite/tools/script.js
+++ staticSite/tools/script.js
@@ -4,7 +4,7 @@ function sendMailToMailChimp() {
   var url = 'https://us14.api.mailchimp.com/3.0/lists/db25ba176d/members/';
 
   var data = {
-    "apikey": "fittshot:" + mailchimp_api_key,
+    "apikey": mailchimp_api_key,
     "email_address": $('#mail-input').val(),
     "status": "subscribed",
     "merge_fields": {
