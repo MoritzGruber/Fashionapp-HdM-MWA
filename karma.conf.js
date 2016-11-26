38b1d9660604aae9321f26991fd334d677bcac88
Moritz Gruber <moritz@Moritzs-MacBook-Air.local>
2016-11-26 20:31:45 +0100

    git commit test

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes t
+    // enable / disable watching file and executing tests whenever any file changes t a
     autoWatch: true,
 
 
