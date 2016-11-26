a7e0e420e54445a2d39b3cd6e6d5e11471705ce9
Moritz Gruber <moritz@Moritzs-MacBook-Air.local>
2016-11-26 20:34:49 +0100

    adsfasf

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes t a sdfasf
+    // enable / disable watching file and executing tests whenever any file changes t a sdfasf asdf
     autoWatch: true,
 
 
