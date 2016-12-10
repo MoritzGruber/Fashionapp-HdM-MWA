5776c93bd8c5436aa59fe954f544b0606b0954ae
MoritzGruber <moritzgruber@yahoo.de>
2016-12-10 12:58:59 +0100

    login works

--- www/js/controller/ProfileCtrl.js
+++ www/js/controller/ProfileCtrl.js
@@ -1,3 +1,13 @@
-angular.module('fittshot.controllers').controller('ProfileCtrl', ['$scope', '$http', function($scope, $http){
+angular.module('fittshot.controllers').controller('ProfileCtrl', ['$scope', '$http', '$localStorage', function($scope, $http, $localStorage){
+
+    $scope.$on('$ionicView.enter', function() {
+     // Code you want executed every time view is opened
+     $scope.username = $localStorage.username;
+     $scope.email = $localStorage.email;
+  })
+    $scope.logout = function(){
+      $localStorage.username = null;
+      $localStorage.email = null;
+    }
 
 }]);
