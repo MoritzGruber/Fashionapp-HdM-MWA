17af1a1230940d6f91e7ee7f68e85682c7f18f7f
Christoph <chrisi.muth@gmx.de>
2017-01-15 13:15:15 +0100

    updated slogan on static page

--- www/js/controller/TabsCtrl.js
+++ www/js/controller/TabsCtrl.js
@@ -1,10 +1,34 @@
 angular.module('fittshot.controllers').controller('TabsCtrl', function ($scope, $rootScope, $location, $localStorage) {
-  if ($localStorage.username === null ){
-    $location.path('/login);');
+    if ($localStorage.username === null) {
+        $location.path('/login);');
 
-  }
-  console.log($localStorage.username);
-  $scope.redirectTo = function (dest) {
-    $location.path('/'+dest);
-  };
+    }
+    $scope.bannerImage = 'img/banners/community.png';
+    console.log($localStorage.username);
+    $scope.showNavigation = false;
+    $scope.redirectTo = function (dest) {
+        $location.path('/' + dest);
+        switch (dest) {
+            case 'community':
+                $scope.indicatorMargin = '0';
+                $scope.bannerImage = 'img/banners/community.png';
+                $scope.bannerHeadline = 'Community';
+                break;
+            case 'collection':
+                $scope.indicatorMargin = '33.333%';
+                $scope.bannerImage = 'img/banners/collection.png';
+                $scope.bannerHeadline = 'Collection';
+                break;
+            case 'profile':
+                $scope.indicatorMargin = '66.666%';
+                $scope.bannerImage = 'img/banners/profile.png';
+                $scope.bannerHeadline = 'Profile';
+        }
+    };
+
+    var navPages = ['/community', '/collection', '/profile'];
+
+    $scope.$on('$routeChangeSuccess', function () {
+        $scope.showNavigation = navPages.indexOf($location.$$path) != -1;
+    });
 });
